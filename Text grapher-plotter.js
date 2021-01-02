6c90a0fadf4fb1095ae9b28e3e08f10997b718e8
Rudxain <76864299+Rudxain@users.noreply.github.com>
2021-01-02 06:22:14 -0400

    Update Text grapher-plotter.js

--- Text grapher-plotter.js
+++ Text grapher-plotter.js
@@ -1,3 +1,6 @@
+//x0 and y0 are the initial scan coordinates. x1 and y1 are the end coords.
+//step is the "resolution". smaller = higher detail.
+//Also, sorry for the unnecessary stuff. I'll clean this up gradually.
 function graph(xpr0=`x`, xpr1=`y`, x0=-6, y0=4, x1=6, y1=-4, step=1/8){
 var dict = {xpr0:xpr0, xpr1:xpr1, x0:x0, y0:y0, x1:x1, y1:y1, step:step}, code = `var x=x0, y=y0, out=[];
 while (y > y1){
