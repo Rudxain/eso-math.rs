9db29a2dbad59c3b88b3272d838c7dbda514e986
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2021-02-06 00:26:19 -0400

    Cleaner, clearer and more efficient code. Also added custom charset support

--- Text grapher-plotter.js
+++ Text grapher-plotter.js
@@ -1,9 +1,12 @@
-//x0 and y0 are the initial scan coordinates. x1 and y1 are the end coords.
+//scan[0, 1] are the initial coordinates, scan[2, 3] are the end coords (x0, y0, x1, y1).
 //step is the "resolution". smaller = higher detail.
-function graph(xpr0=`x`, xpr1=`y`, x0=-6, y0=4, x1=6, y1=-4, step=1/8){
-return eval(`var x = ${x0}, y = ${y0}, out=[];
-while (y > ${y1}){
-   if (x >= ${x1}) {y -= ${step}};
-   if (x < ${x1}) {x += ${step}} else {x = ${x0}}
-   out.push(\`\${((${xpr1}) + ${step}/2 > ${xpr0} && (${xpr1}) - ${step}/2 < (${xpr0})) ? "#" : ((${xpr1}) + ${step} > ${xpr0} && (${xpr1}) - ${step} < (${xpr0})) ? "+" : " "}\${(x < ${x1}) ? "" : "\\n"}\`);
-}; out.join("")`)}
+//pix is the charset for approximation intensity levels, in "little-endian" order.
+function plot(xprA=`x`, xprB=`y`, scan=[-6, 4, 6, -4], step=1/8, pix=' +#')
+{return eval(`var out='';
+    for (let y = ${scan[1]}; y >= ${scan[3]}; y -= ${step})
+    {
+        for (let x = ${scan[0]}; x <= ${scan[2]}; x += ${step})
+        {out += ((((${xprB}) + ${step/2} > (${xprA}) && (${xprB}) - ${step/2} < (${xprA})) ? "${pix[2]}" : ((${xprB}) + ${step} > (${xprA}) && (${xprB}) - ${step} < (${xprA})) ? "${pix[1]}" : "${pix[0]}") + (x < ${scan[2]} ? "" : "\\n"))}
+    }
+    out`)
+}
