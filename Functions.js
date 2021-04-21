4dae848a91ab8cee2070616e76d6296455cd4882
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2021-04-21 00:28:35 -0400

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,13 +1,54 @@
-Math.PHI = (1 + Math.sqrt(5)) / 2;
+'use strict';
 
-function Big_sign(n) {return n == 0n ? 0n : (n < 0n ? -1n : 1n)}
+Math.TAU = 2 * Math.PI;
+Math.SQRT3 = Math.sqrt(3);
+Math.SQRT5 = Math.sqrt(5);
+Math.PHI = (1 + Math.SQRT5) / 2;
+Math.LNPHI = Math.log(Math.PHI);
+Math.LOG2PHI = Math.log2(Math.PHI);
+Math.LOG10PHI = Math.log10(Math.PHI);
+Math.logPHI = function (x) {return Math.log2(x) / Math.LOG2PHI};
+//I'm using lb for better precision and performance than ln
+Math.LOGPHI2 = Math.logPHI(2);
+Math.LOGPHI_E = Math.logPHI(Math.E);
+Math.LOGPHI10 = Math.logPHI(10);
 
-function Big_abs(n) {return n * Big_sign(n)}
+function Big_sign(n) {return n ? (n < 0n ? -1n : 1n) : 0n}
 
-//Converts a number N into a numeral of base B using an alternative method
+function Big_abs(n) {return n < 0n ? -n : n}
+
+//f < 0 = floor
+//f > 0 = ceil
+//falsy f = round
+//floor and ceil on negative inputs return incorrect values, sorry
+//truncated division (int div) is JS' default for BigInts
+function Big_div(a, b, f) {return a % b ? (f ? ((a + b - 1n) / b) + (f < 0n ? -1n : 0n) : (b / 2n + a) / b) : a / b}
+
+function Big_root(n, i=2n)
+{
+    let sign = Big_sign(n);
+    if (sign === -1n && !(i & 1n)){return NaN} //NaN is placeholder for imaginary
+    n = Big_abs(n);
+    if (!n || n == 1n) {return n}
+    let s = n + 1n;
+    let k = i - 1n;
+    let u = n;
+    while (u < s) {s = u; u = (u * k + n / u ** k) / i}
+    return s * sign
+}
+
+//I don't know if lb is really better but I use it anyways, lol
+Math.logB = function (x, b=Math.E) {return Math.log2(x) / Math.log2(b)}
+
+//This isn't very efficient
+function Big_logB(n, b=2n) {let i = 0n; while (n > 1n) {n /= b; i++} return i}
+
+
+//The following need to be enhanced/upgraded for better usefulness than JS' built-in implementations
+//Converts a number N into a numeral of base B using an alternative algorithm
 function toNumeralAlt(N, B, charset=[`0`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`,`A`,`B`,`C`,`D`,`E`,`F`,`G`,`H`,`I`,`J`,`K`,`L`,`M`,`N`,`O`,`P`,`Q`,`R`,`S`,`T`,`U`,`V`,`W`,`X`,`Y`,`Z`])
 { 
-    var charset = (Array.isArray(charset) ? charset : charset.split(``)), out=[];
+    let out = [];
     for (i=0; i < (B > 1 ? Math.floor(Math.log(N) / Math.log(B)) + 1 : N); i++)
     {
         out.unshift(charset[B > 1 ? (N % B**(i+1) - N % B**i) / B**i : 1])
@@ -25,23 +66,55 @@ function toNumeral(N, B) {dig = []; while (N > 0) {dig.unshift(N % B); N = N / B
 function parseNumeral(numeral, B) {var n=0; for (d of numeral) {n = b * n + d} return n}
 
 //Convert the digits representation of a number from a base/radix to another
-function Base2Base(numeral, InB, OutB) {return toNumeral(parseNumeral(numeral, InB), OutB)}
+function B2B(numeral, inpB, outB) {return toNumeral(parseNumeral(numeral, inpB), outB)}
+//End of "the following". The next "followings" are more useful:
 
-//Digital Root. Returns the sequence of values for each iteration
-//If m is truthy, it usea multiplicative mode instead of additive
+
+//en.wikipedia.org/wiki/Digital_root#Congruence_formula
+function dig_rt_c(n, b){return n ? 1 + ((n - 1) % (b - 1)) : 0}
+
 //en.wikipedia.org/wiki/Digital_root
-function dig_rt(n, m)
+function dig_sum(x, b)
 {
-    var out = [], tmp = 0
-    while (n.length > 1)
-    {
-        out.push(tmp + '' || n)
-        for (let i = 0; i < n.length; i++) {tmp = (m ? (i ? tmp : 1) * n[i] : (i && tmp) + n[i])}
-    }
-    return out
+    let sum = 0;
+    while (x > 0) {sum += x % b; x = Math.trunc(x / b)}
+    return sum
 }
 
-//elegant (no auxiliary var) GCD, with support for signeds
+//persistence = seen.length - 1
+function dig_rt_a(x, b)
+{
+    let seen = [];
+    while (!(seen.includes(x))) {seen.push(x); x = dig_sum(x, b)}
+    return {'x': x, 'seen': seen} //FOR DEBUG
+}
+
+//en.wikipedia.org/wiki/Multiplicative_digital_root
+function dig_prod(x, b)
+{
+    if (!x) {return x}
+    let prod = 1;
+    while x > 1
+    {
+        if (!(x % b)) {return 0}
+        if (x % b > 1) {prod *= x % b}
+        x = Math.trunc(x / b)
+    }
+    return prod
+}
+
+function dig_rt_m(x, b)
+{
+    let seen = [];
+    while (!(seen.includes(x))) {seen.push(x); x = dig_prod(x, b)}
+    return {'x': x, 'seen': seen} //FOR DEBUG
+}
+
+//get non-integer part
+function frac_m(x) {return x % 1}
+//I have no idea which is better
+function frac_t(x) {return x - Math.trunc(x)}
+
 function gcd(a, b)
 {
     a = Math.abs(a); b = Math.abs(b);
@@ -50,7 +123,6 @@ function gcd(a, b)
     return r[0]
 }
 
-//GCD for BigInt, also supports signeds
 function Big_gcd(a, b)
 {
     a = Big_abs(a); b = Big_abs(b);
@@ -60,118 +132,113 @@ function Big_gcd(a, b)
     return a
 }
 
-function lcm(a, b) {return ((typeof a == 'bigint' ? Big_abs(a) : Math.abs(a)) / gcd(a, b)) * (typeof b == 'bigint' ? Big_abs(b) : Math.abs(b))}
+function lcm(a, b) {return Math.abs(a) / gcd(a, b) * Math.abs(b)}
+function Big_lcm(a, b) {return Big_abs(a) / Big_gcd(a, b) * Big_abs(b)}
 
-var Pa = [2, 3, 5] // Array of Primes
-var Pd = {2:true, 3:true, 5:true} //Primality dictionary
+var Pa = [2, 3, 5]; // Array of Primes
+var Pd = new Map(); Pd.set(2); Pd.set(3); Pd.set(5) //Primality map/dictionary
 function addP()
 {
-    let x = Pa[Pa.length - 1] + 2, y = Math.sqrt(x);
-    for (let j = 1; true; x += 2, y = Math.sqrt(x))
+    let x = Pa[Pa.length - 1] + 2;
+    test:
+    for (let y = Math.sqrt(x), j; true; x += 2, y = Math.sqrt(x))
     {
-        if (y == Math.trunc(y)) {continue} //ignore squares because they are composite
+        if (Pd.has(x)) {break}
+        if (Number.isInteger(y)) {continue} //ignore squares because they are composite
         j = 1;
-        while (Pa[j] <= y && (x % Pa[j])) {j++}
-        if (!(x % Pa[j])) {continue}
-        Pa.push(x); Pd[x] = true; break;
+        while (Pa[j] <= y) {if (x % Pa[j] === 0) {continue test} j++}
+        Pd.set(x); break;
     }
+    Pa.push(x)
 }
-function factorize(n)
+function factor(n) //get prime factorization of n
 {
     n = Math.abs(n);
     let m = n, i = 0, out = [];
-    while (Pa[i] <= Math.sqrt(n) && Pa[i] <= m && !Pd[m]) //Trust me, all 3 are necessary for speed
+    while (Pa[i] <= Math.sqrt(n) && Pa[i] <= m && !(Pd.has(m))) //Trust me, all 3 are necessary for speed
     {
-        while (!(m % Pa[i])) {m /= Pa[i]; out.push(Pa[i])}
+        while (m % Pa[i] === 0) {m /= Pa[i]; out.push(Pa[i])}
         i++;
-        if (i >= Pa.length) {addP()} //Primes on-demand, like modern TV lol
+        if (i >= Pa.length) {addP()} //Primes on-demand lol
     }
-    if (m != 1){out.push(m); Pd[m] = true}
+    if (m > 1) {out.push(m); Pd.set(m)}
     return out
 }
 
-//Factorial for Reals
-function Stirling(x) {return Math.sqrt(2 * Math.PI * x) * (x / Math.E)**x}
+//factorial approximation for non-integers
+function Stirling(x) {return Math.sqrt(Math.TAU * x) * (x / Math.E) ** x}
 
-//"pseudo-recursive" (perhaps "semi-recursive") Fact with support for any number (except BigInt and Imaginary)
+//"pseudo-recursive" (perhaps "quasi-recursive") Fact with support for any number (except BigInt and Imaginary)
+//returns array because I still need to add a more efficient F to compute just 1 value
+//this is optimized for returning an array
 function Fact(x)
 {
-    let out = [1], i = (x % 1) + Math.sign(x);
-    while (out.length <= Math.abs(x))
-    {
-        out.push(i * out[out.length - 1]); i += Math.sign(x)
-    }
+    let out = [1];
+    for (let i = (x % 1) + Math.sign(x); out.length <= Math.abs(x); i += Math.sign(x))
+    {out.push(i * out[out.length - 1])}
     return out
 }
 
-//Iterative inverse Fact. If k > 1 returns corresponding inv multifactorial
-function invFact(n, k=1)
+//iterative inverse Fact. If k > 1 returns corresponding inv multifactorial
+function invFact(n, k = 1)
 {
-    var x = 1;
-    while (n > 1) {x += k; n /= x}
+    if (!n) {return NaN}
+    let x = (n % 1) + Math.sign(n);
+    while (Math.abs(n) > 1) {x += k; n /= x}
     return x
 }
 
 //"Termial/Additorial/Sumatorial" Fs
-//Info: en.wikipedia.org/wiki/Triangular_number ; en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+//en.wikipedia.org/wiki/Triangular_number ; en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
 
-//Get Nth (x) "TriNumber" in O(1). If "f" is truthy, returns inverse
-function trinum(x, f) {return (f ? -1 + Math.sqrt(1 + 8*x) : x*(x+1)) / 2}
+//get Nth "TriNumber" in O(1)
+function trinum(x) {return x * (x + 1) / 2}
 
-//Get TriNums up to index x (inclusive)
+//get index of a trinum
+function tri_inv(x) {return (Math.sqrt(8 * x + 1) - 1) / 2}
+
+//get TriNums up to index x (inclusive)
 function triseq(x)
 {
-    let out = [0], i = (x % 1) + Math.sign(x);
-    while (out.length <= Math.abs(x))
-    {
-        out.push(i + out[out.length - 1]); i += Math.sign(x)
-    }
+    let out = [0];
+    for (let i = (x % 1) + Math.sign(x); out.length <= Math.abs(x); i += Math.sign(x))
+    {out.push(i + out[out.length - 1])}
     return out
 }
 
 
-//Info: en.wikipedia.org/wiki/Polygonal_number
-function ppn(i, s=3) {return ((s - 2) * i**2 - (s - 4) * i) / 2}
-function ipn(p, s=3) {return (Math.sqrt(8 * (s - 2) * p + (s - 4)**2) + (s - 4)) / (2 * (s - 2))}
+//en.wikipedia.org/wiki/Polygonal_number
+function ppn(i, s=3) {return (s - 2) * i**2 - (s - 4) * i) / 2}
+function ipn(p, s=3) {return Math.sqrt(8 * (s - 2) * p + (s - 4)**2) + (s - 4)) / (2 * (s - 2))}
 function spn(p, i=2) {return 2 + (2 / i) * ((p - i) / (i - 1))}
 
-//Get Nth Fibonacci number in O(1)
-function Fib1(n) {return Math.round(Math.PHI ** n / Math.sqrt(5))}
+//get Nth Fibonacci in O(log(n))
+function Fib(n) {return Math.round(Math.PHI ** n / Math.SQRT5)}
 
-//Inverse Fib, returns the index of a Fib num
-function invFib(F) {return Math.floor(Math.log(F * Math.sqrt(5) + 0.5) / Math.log(Math.PHI))}
+//get index of a Fib num
+function invFib(F) {return F <= 1 ? F : Math.floor(Math.logPHI(F * Math.SQRT5 + 0.5))}
 
-//Info: en.wikipedia.org/wiki/Lucas_sequence
+//en.wikipedia.org/wiki/Lucas_sequence
 //If F is falsy (default) then "U", else "V"
-//Default returns Fib seq
 function Lucas(n, P=1, Q=-1, F)
 {
-    var seq = (F ? [2,P] : [0,1]);
-    for (i=1; i<n; i++) {seq.push(P * seq[i] - Q * seq[i-1])}
+    let seq = (F ? [2, P] : [0, 1]);
+    for (let i = 1; i < n; i++) {seq.push(P * seq[i] - Q * seq[i-1])}
     return seq
 }
 
 //Riemann Zeta F
-function zeta(s, lim=512)
+function zeta(s, k=2)
 {
-    var sum = 0;
-    if (s > 1)
-    {
-        let tmp = 0;
-        for (let n=1; true; n++)
-        {
-            sum += 1 / (n**s);
-            if (sum === tmp) {break}
-            tmp += 1 / (n**s)
-        }
-    }
-    else {for (let n=1; n<lim; n++) {sum += 1 / (n**s)}}
+    let sum = 1, n = 2;
+    if (s > 1 && k <= 2) {for (let tmp; sum !== tmp; n++) {tmp = sum; sum += 1 / n**s}}
+    else {while (n <= k) {sum += 1 / n**s; n++}}
     return sum
 }
 
 //Generalized Collatz
 //en.wikipedia.org/wiki/Collatz_conjecture#Undecidable_generalizations
-function genCol(n, k=2, a=[0.5,3], b=[0,1], P=2)
+function genCol(n, k=2, a=[0.5, 3], b=[0, 1], P=2)
 {
     let seq = [n], i = 0;
     while (seq.length < k)
@@ -192,15 +259,14 @@ en.wikipedia.org/wiki/Collatz_conjecture
 */
 function Collatz(n, k, s)
 {
-    var h = [n];
-    while (k ? h.length < k : !{0:true, 1:true, '-1':true, '-5':true, '-17':true}[h[h.length - 1]])
-    {
-        h.push(h[h.length - 1] % 2 ? (3 * h[h.length - 1] + 1) / (s ? 2 : 1) : h[h.length - 1] / 2)
-    }
+    let h = [n];
+    const col_c = new Map(); col_c.set(0); col_c.set(1); col_c.set(-1); col_c.set(-5); col_c.set(-17);
+    while (k ? h.length < k : !(col_c.has(h[h.length - 1])))
+    {h.push(h[h.length - 1] & 1 ? (3 * h[h.length - 1] + 1) / (s ? 2 : 1) : h[h.length - 1] / 2)}
     return h
 }
 
-//Unary-Numeral Ackermann
+//Unary-Numeral (Base 1) Ackermann
 function UAck(m, n)
 {
     while (m.length > 3)
@@ -208,35 +274,35 @@ function UAck(m, n)
         n = n.length ? UAck(m, n.slice(0, -1)) : '1';
         m = m.slice(0, -1)
     }
-    if (m.length == 3) {let len = n.length; while (n.length < 2**(len + 3) - 3) {n += '1'}}
+    if (m.length == 3) {let len = 2 ** (n.length + 3) - 3; while (n.length < len) {n += '1'}}
     else {n = [n + '1', n + '11', n + n + '111'][m.length]}
     return n
 }
 
-function Madlatz(x, y)
+function Madlatz(m, n)
 {
-    while (x !== 1)
+    while (m > 1n)
     {
-        y = y === 0 ? 1 : Madlatz(x, y-1);
-        x = (x % 2 ? 3 * x + 1 : x / 2)
+        if (n) {n = Madlatz(m, n-1n)} else {n = 1n}
+        m = (m & 1n ? 3n * m + 1n : m / 2n)
     }
-    return y+1
+    return n+1n
 }
 
-function Coolman(x, y)
+function Coolman(m, n)
 {
-    while (x)
+    while (m)
     {
-        if (y < 2) {y = 4}
-        else {let tmp = Coolman(x, y-1); y = tmp % 2 ? 3 * tmp + 1 : tmp / 2}
-        x--
+        if (n < 2n) {n = 4n}
+        else {let tmp = Coolman(m, n-1n); n = tmp & 1n ? 3n * tmp + 1n : tmp / 2n}
+        m--
     }
-    return y+1
+    return n+1
 }
 
 var Ack_mem = {};
 function Ackermann(m, n)
-{'use strict';
+{
     while (m > 3)
     {
         if (!n) {n = 1n}
@@ -245,7 +311,7 @@ function Ackermann(m, n)
                 if (Ack_mem[m + ',' + n]) {return Ack_mem[m + ',' + n]}
                 n--;
                 if (Ack_mem[m + ',' + n]) {n = Ack_mem[m + ',' + n]}
-                else {let out = Ackermann(m, n); Ack_mem[m + ',' + n] = out; n = out}
+                else {let tmp = Ackermann(m, n); Ack_mem[m + ',' + n] = tmp; n = tmp}
             }
         m--
     }
@@ -256,4 +322,4 @@ function Ackermann(m, n)
 function ack_inv(m, n) {for (var i=1, x=0; x < Math.log2(n); i++) {x = Ackermann(i, BigInt(Math.trunc(m / n)))}; return x}
 
 //en.wikipedia.org/wiki/Amdahl%27s_law
-function Ams(p, s) {return 1/((1-p) + (p/s))}
+function Ams(p, s) {return 1 / ((1 - p) + (p / s))}
