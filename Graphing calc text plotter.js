d10127cb0ef3a322dd631909c85b9d144a2fed21
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2021-10-01 17:41:37 -0400

    Update Graphing calc text plotter.js

--- Graphing calc text plotter.js
+++ Graphing calc text plotter.js
@@ -5,10 +5,11 @@
 //pix is the charset for approximation intensity levels, in "little-endian" order.
 function plot(xprA = 'x', xprB = 'y', scan = [-6, 4, 6, -4], step = 1 / 8, pix = ' +#')
 {
-   [xprA, xprB, scan, step, pix] = ['('+ xprA +')', '('+ xprB +')', scan.map(x => Number(x)), Number(step), String(pix)];
+   [xprA, xprB, scan, step, pix] = ['('+ xprA +')', '('+ xprB +')', scan.map(x => Number(x) || 0), Number(step) || 1, typeof pix === 'array' ? pix.join('') : String(pix)];
    return Function(`let out = '';
       for (let y = ${scan[1]}; y >= ${scan[3]}; y -= ${step})
          for (let x = ${scan[0]}; x <= ${scan[2]}; x += ${step})
             out += '${pix}'[(${xprB} + ${step} > ${xprA} && ${xprB} - ${step} < ${xprA}) + (${xprB} + ${step} / 2 > ${xprA} && ${xprB} - ${step} / 2 < ${xprA})] + (x < ${scan[2]} ? '' : '\\n');;
       return out`)()
 }
+//TO-DO: fix the bug that happens when `pix` contains "'"
