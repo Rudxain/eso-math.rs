4777c0b8466210371e56616346f58f672c1fd342
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-08-11 13:00:18 -0400

    Update Collatz.js

--- src/lib/Collatz.js
+++ src/lib/Collatz.js
@@ -1,9 +1,9 @@
 import '../typedefs'
-import {isInfNaN} from '../helper/value check'
+import {toNumeric} from '../helper/sanitize'
 import {trunc} from './rounding'
 import {lcm} from './factors'
 
-const Float = Number, Int = BigInt, _Set = Set
+const Float = Number, Int = BigInt, Set_ = Set, Arr = Array
 
 /**
 generalized Collatz
@@ -12,12 +12,11 @@ https://en.wikipedia.org/wiki/Collatz_conjecture#Undecidable_generalizations
 */
 export const Collatz_gen = function* (kwargs)
 {
-	let {n} = kwargs
-	const {a = [[1, 2], [3, 1]], b = [[0, 1], [1, 1]], P = 2} = kwargs
+	let {n, a = [[1, 2], [3, 1]], b = [[0, 1], [1, 1]], P = 2} = kwargs
 	n = toNumeric(n)
+	a = Arr(a).map(x => Arr(x).map(toNumeric))
+	b = Arr(b).map(x => Arr(x).map(toNumeric))
 	P = trunc(toNumeric(P))
-
-
 
 	/**
 	add 2 fractions. format is [num, den]
@@ -30,9 +29,9 @@ export const Collatz_gen = function* (kwargs)
 		return [x / f0[1] * f0[0] + x / f1[1] * f1[0], x]
 	}
 	/**
-	converts a fraction to its expanded binary form
-	@param {fraction} f frac to convert
-	@return {numeric} expanded frac
+	converts a fraction to its "binary" expansion form
+	@param {fraction} f
+	@return {numeric}
 	*/
 	const frac_to_num = f => f[0] / f[1]
 
@@ -61,7 +60,7 @@ export const Collatz_std = (n, k, s) => {
 	if (typeof n == 'bigint')
 	{
 		h[h.length] = n
-		const CYCLES = new _Set([1n, 0n, -1n, -5n, -17n])
+		const CYCLES = new Set_([1n, 0n, -1n, -5n, -17n])
 		while (k ? h.length < k : !CYCLES.has(h.at(-1)))
 			h[h.length] = h.at(-1) & 1n
 				? (3n * h.at(-1) + 1n) / (s ? 2n : 1n)
@@ -70,7 +69,7 @@ export const Collatz_std = (n, k, s) => {
 	else
 	{
 		h[h.length] = trunc(n)
-		const CYCLES = new _Set([1, 0, -1, -5, -17, Infinity, -Infinity, NaN])
+		const CYCLES = new Set_([1, 0, -1, -5, -17, Infinity, -Infinity, NaN])
 		while (k ? h.length < k : !CYCLES.has(h.at(-1)))
 			h[h.length] = h.at(-1) % 2
 				? (3 * h.at(-1) + 1) / (s ? 2 : 1)
