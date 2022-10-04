bd6b24f5b6243ba67844198c177a1f52aee47bce
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-03 20:24:10 -0400

    add npm meta, eslintrc, export more from index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
+import * as con from './lib/const'
 import * as std from './lib/std'
+import * as mean from './lib/mean'
 import * as round from './lib/rounding'
-import * as bit from './lib/bitwise'
-import * as rand from './lib/random'
+import * as fac from './lib/factorial'
+import * as facts from './lib/factors'
+import * as bit_cast from './lib/bit cast'
+import * as bitwise from './lib/bitwise'
+import * as m from './lib/Mersenne'
+import * as rng from './lib/random'
+import * as pow from './lib/power'
+import * as hyper from './lib/hyper'
+import * as drt from './lib/digit root'
+import * as dec from './lib/decay'
+import * as pron from './lib/pronic'
+import * as luke from './lib/Lucas'
+import * as coll from './lib/Collatz'
+
+//is there a better way to do this?
+const modules = [
+	con,
+	std,
+	mean,
+	round,
+	fac,
+	facts,
+	bit_cast,
+	bitwise,
+	m,
+	rng,
+	pow,
+	hyper,
+	drt,
+	dec,
+	pron,
+	luke,
+	coll
+]
 
 const NTML = {}
-for (const O of [std, round, bit, rand])
+for (const O of modules)
 	Object.assign(NTML, O)
 export default NTML
--- src/lib/decay.js
+++ src/lib/decay.js
@@ -4,18 +4,35 @@ import {signabs} from './std'
 //Division by repeated subtraction, but the divisor gets decremented each iteration
 export const decayDiv = (n, d) => {
 	[n, d] = [signabs(n), signabs(d)]
+
 	let i = isIntN(n[0]) ? 0n : 0
-	while (n[1] > d[1]) {n[1] -= d[1]--; i++}
-	return [n[0] * i, n[0] * n[1], d[0] * d[1]]
+
+	while (n[1] > d[1]) {
+		n[1] -= d[1]--
+		i++
+	}
+
 	//quotient, remainder, and unnamed, lol
+	return [n[0] * i, n[0] * n[1], d[0] * d[1]]
 }
 
-//Multiplication by repeated addition, but multiplier gets decremented
-export const decayMult0 = (f, m) => {
-	[f, m] = [signabs(f), signabs(m)]
-	if (!f[0] || !m[0]) return f[0] * m[0] //if NaN or 0 then No-Op
-	let i = isIntN(m[0]) ? 0n : 0, x = f[1]
-	while (i < m[1]) {x += f[1]; m[1]--; i++}
+/**
+Multiplication by repeated addition, but multiplier gets decremented
+*/
+export const decayMult0 = (f, d) => {
+	[f, d] = [signabs(f), signabs(d)]
+
+	if (!f[0] || !d[0])
+		return f[0] * d[0]
+
+	let i = isIntN(d[0]) ? 0n : 0, n = f[1]
+
+	while (i < d[1]) {
+		n += f[1]
+		d[1]--
+		i++
+	}
+
 	return [n[0] * i, n[0] * n[1], d[0] * d[1]]
 }
 
