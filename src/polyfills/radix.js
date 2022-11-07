2534ddfaa7f1709790ea179f6fe93754435ea74a
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-07 02:28:02 -0400

    delete `BigInt.parse` #29

--- src/polyfills/radix.js
+++ src/polyfills/radix.js
@@ -19,45 +19,4 @@ defProp(Number.prototype, 'toScientific', function toScientific(b = 10) {
 	return x.toString(b) + ` * 10^${e.toString(b)} (base 0d${b})`
 }, 0b101)
 
-//https://tc39.es/ecma262/multipage/global-object.html#sec-parseint-string-radix
-BigInt.parse = function(string, radix) {
-	string = Str(string).trimStart().toLowerCase()
-	let sign = 1n
-	if (string) {
-		switch (string[0]) {
-			case '-': sign = -1n
-			case '+': string = string.substring(1)
-		}
-	}
-	let stripPrefix = true
-	if (radix = Float(radix) | 0) {
-		if (radix < 2 || radix > 36) throw new RangeErr('Invalid base')
-		if (radix != 0x10) stripPrefix = false
-	}
-	else radix = 10;
-
-	if (stripPrefix &&
-		string.length >= 2 &&
-		string[0] == '0' &&
-		string[1] == 'x')
-		{string = string.substring(2); radix = 0x10}
-
-	const charset = new _Map
-	for (let i = 0n; i < radix; i++)
-		charset.set('0123456789abcdefghijklmnopqrstuvwxyz'[i], i)
-
-	let end = -1
-	while (++end < string.length)
-		if (!charset.has(string[end])) break
-
-	string = string.substring(0, end)
-	let int = 0n
-	if (!string) return int //no need to throw, 0 fits better
-	radix = IntN(radix); end = IntN(end)
-	//DO NOT REVERSE iteration order
-	for (let i = end - 1n; i >= 0; i--)
-		int += charset.get(string[end - i - 1n]) * radix ** i
-	return sign * int
-}
-
 export {Number, BigInt}
