596b160da63696adec16938799cf16f332f6f12d
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-07 07:18:11 -0400

    Update bit cast.js

--- src/lib/bit cast.js
+++ src/lib/bit cast.js
@@ -1,14 +1,12 @@
 const IntNArr = BigUint64Array, FloatArr = Float64Array
 /**
 get the internal bits (binary64 IEEE 754 representation)
-@param {number} number
-@return {bigint}
+@param {number} f
 */
-export const F64toI64 = f => new IntNArr(new FloatArr([f]).buffer)[0]
+export const F64_to_I64 = f => new IntNArr(new FloatArr([f]).buffer)[0]
 
 /**
 mask the 64 LSBs and read as IEEE-754 binary64 floating-point format
 @param {bigint} n
-@return {number}
 */
-export const I64toF64 = n => new FloatArr(new IntNArr([n]).buffer)[0]
+export const I64_to_F64 = n => new FloatArr(new IntNArr([n]).buffer)[0]
