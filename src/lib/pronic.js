a8a9eef1c2245e5ac1f813d4b8c0e6c2ed55023b
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-11-10 10:47:36 -0400

    Update pronic.js

--- src/lib/pronic.js
+++ src/lib/pronic.js
@@ -1,28 +1,38 @@
-import {round} from './rounding'
+/** https://en.wikipedia.org/wiki/Pronic_number */
+import { take } from '../mod/iter'
+import { round } from './rounding'
 
-//https://en.wikipedia.org/wiki/Pronic_number
+const Float = Number
+
+export const isPronic = x => { }
+
 export const nthP = n => n * ++n //supports any numeric type
-export const iterP = function*(b) {
+export const iterP = function* (b) {
 	const n2 = b ? 2 : 2n
 	let sum = n2, p = n2 ^ n2 //auto 0
 	yield p
-	while (true) {yield p += sum; sum += n2}
+	while (true) { yield p += sum; sum += n2 }
 }
-//list recursive Pronics
-export const iterRecurP = function*(b) {
-	let p = b ? 2 : 2n
+
+/** list recursive Pronics */
+export const iterRecurP = function* () {
+	let p = 2n
 	yield p
-	while (true) yield p *= p + (b ? 1 : 1n)
+	while (true) yield p *= p + 1n
 }
-//the Doubly-Pronic (recursive) constant
+
+/** the Doubly-Pronic (recursive) constant */
 export const RecurPronicCONST = (() => {
 	let sum = 0
-	const G = iterRecurP()
-	for (const _ of Array(7)) sum += 1 / Number(G.next().value)
+	for (const x of take(iterRecurP(), 7n))
+		sum += 1 / Float(x)
 	return sum
 })()
-//int Geometric Progression where ratio between terms approaches the reciprocal of the Doubly-Pronic constant
-export const iterRecurPConstInv = function*() {
+
+/**
+int Geometric Progression where ratio between terms approaches `1 / RecurPronicCONST`
+*/
+export const iterRecurPConstInv = function* () {
 	let p = 2
 	yield p
 	while (true) yield p = round(p / RecurPronicCONST)
