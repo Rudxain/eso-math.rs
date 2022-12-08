26707a68426d5d55624290f611d715a4188e172c
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-08 18:10:49 -0400

    fix types in sanitize

--- src/mod/sanitize.js
+++ src/mod/sanitize.js
@@ -14,20 +14,23 @@ const
 	Str = String,
 	TypeErr = TypeError
 
+/**{@link Number.MAX_SAFE_INTEGER}*/
+const MAX_SAFE_INT = /**@type {9007199254740991}*/(Float.MAX_SAFE_INTEGER)
+
 /**
 `copyType` (like `copySign`), but only for Numericals.
 This is like type-inference
 
 @template {numeric} T
-@param {*} n value to coerce
+@param {numstr|boolean} n value to coerce
 @param {T} x from which the type is copied
-@return {T}
 */
-export const autoN = (n, x) => (typeof x == 'bigint' ? IntN : Float)(n)
+export const autoN = (n, x) => /**@type {T}*/((typeof x == 'bigint' ? IntN : Float)(n))
 
 /**
 https://tc39.es/ecma262/multipage/abstract-operations.html#sec-tobigint
-@param {boolean | string | bigint} x
+@template {boolean|string|bigint} T
+@param {T} x
 */
 export const toBigInt = x => {
 	switch (typeof x?.valueOf?.()) {
@@ -40,39 +43,42 @@ export const toBigInt = x => {
 
 /**
 permissive BigInt coercion
-@param {*} x value to coerce
+@template T
+@param {T} x value to coerce
 @return {bigint}
 */
 export const anyBigInt = x => {
-	if (isIntN(x)) return IntN(x)
+	if (isIntN(x)) return IntN(/**@type {bigint}*/(x))
 	if (isFloat(x)) {
-		x = trunc(+x)
-		return x != 0 ? (isInf(x) ? (x < 0 ? -1n : 1n) : IntN(x)) : 0n
+		const n = trunc(+x)
+		return n != 0 ? (isInf(n) ? (n < 0 ? -1n : 1n) : IntN(n)) : 0n
 	}
-	if (!(x && (x = x?.valueOf?.()))) return 0n
-	if (!isPrim(x)) return 1n
-	return IntN(x)
+	const v = x?.valueOf?.()
+	if (!(x && v)) return 0n
+	if (!isPrim(v)) return 1n
+	return IntN(/**@type {string|true}*/(v))
 }
 
 /**
 Coerce to numeric by using the least invasive/intrusive algorithm I know.
 DO NOT confuse with ES' `toNumeric` "abstract operation", it's not the same.
-@param {*} x value to coerce
+@template T
+@param {T} x value to coerce
 @return {numeric}
 */
 export const toNumeric = x => {
 	if (isFloat(x)) return +x
-	if (isIntN(x)) return IntN(x)
+	if (isIntN(x)) return IntN(/**@type {bigint}*/(x))
 	if (x === null) return 0
 
-	x = x?.valueOf?.()
-	if (x === undefined || typeof x == 'symbol')
+	let v = x?.valueOf?.()
+	if (v === undefined || typeof v == 'symbol')
 		return NaN
 
-	if (!isPrim(x)) x = Str(x)
+	if (!isPrim(v)) v = Str(v)
 
-	if (!+x || abs(+x) < 2 ** 53 ||
+	if (!+v || abs(+v) <= MAX_SAFE_INT ||
 		//I know /\s/ exists, but `trim` is faster and more readable
-		/^[-+]?Infinity$/.test(Str(x).trim())) return +x
-	return IntN(typeof x == 'string' && x.includes('.') ? x.substring(0, x.indexOf('.')) : x)
+		/^[-+]?Infinity$/.test(Str(v).trim())) return +v
+	return IntN(typeof v == 'string' && v.includes('.') ? v.substring(0, v.indexOf('.')) : /**@type {string|boolean}*/(v))
 }
