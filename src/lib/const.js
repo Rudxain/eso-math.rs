bba52359638f1332628d68633f4625bf89306781
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-11 16:42:26 -0400

    move consts to internal module

--- src/lib/const.js
+++ src/lib/const.js
@@ -1,14 +1,3 @@
-import {M as nthMersenne} from './Mersenne'
-
-export const
-	/** 2pi */
-	TAU = Math.PI * 2,
-	SQRT5 = 5 ** 0.5,
-	/** The Golden Ratio */
-	PHI = SQRT5 / 2 + 0.5,
-	/** maximum 64bit unsigned int */
-	MAX64 = nthMersenne(0x40n)
-
 /** sum of inverse `pow`s of itself */
 export const invPowSum = (() => {
 	let i = 0, out = 0, tmp
--- src/mod/const.js
+++ src/mod/const.js
@@ -1,3 +1,16 @@
+import { M as nthMersenne } from "../lib/Mersenne"
+
+/** 2pi */
+export const TAU = Math.PI * 2
+
+export const
+	SQRT5 = 5 ** 0.5,
+	/** The Golden Ratio */
+	PHI = SQRT5 / 2 + 0.5
+
+/** maximum 64bit unsigned int */
+export const MAX64 = nthMersenne(0x40n)
+
 /**
 bit length of IEEE-754 binary64 float significand.
 
