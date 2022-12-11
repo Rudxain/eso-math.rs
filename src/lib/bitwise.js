cf70dc2248e80846d7d5a001beb7eec3be572def
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-11 14:52:07 -0400

    refactor bitwise

--- src/lib/bitwise.js
+++ src/lib/bitwise.js
@@ -18,27 +18,29 @@ if `word_size` = 1 then: 0: lb, 1: length (ignore sign), 2: length (include sign
 */
 export const sizeOf = (n, word_size = 1n, init = 0n) => {
 	n = abs(n)
-	//eslint-disable-next-line no-constant-condition
-	while (true) {
-		n >>= word_size
-		if (n == 0n) break
-		init++
-	}
+	while (n >>= word_size) init++
 	return init
 }
+
+/**
+maximum float exponent
+{@link Math.log2}({@link Number.MAX_VALUE}) = ilb(2^1024 - 1) + 1
+*/
+const MAX_EXP = 0x400
 
 /**
 count trailing zeros in binary.
 extended to non-ints, so negative exponents are supported.
 @template {numeric} T
 @param {T} n
-@return {[T, T]} 2-tuple format [CTZ, trimmed `n`]
+@return {[numeric, T]} 2-tuple format [CTZ, trimmed `n`]
 */
 export const ctztrim = n => {
-	if (n === 0) return [0x400, n] //(rounded) `Math.log2(Number.MAX_VALUE)` = (truncated) ilb(2 ^ 1024 - 1) + 1
+	if (n === 0) return [MAX_EXP, n]
 	if (n === 0n) return [Infinity, n]
 
-	const B = isIntN(n), n1 = B ? 1n : 1
+	const B = typeof n == 'bigint'
+	const n1 = /**@type {T extends bigint ? 1n : 1}*/(B ? 1n : 1)
 	let c = autoN(0, n)
 
 	if (!B && !isInt(+n)) {
@@ -56,7 +58,6 @@ export const ctztrim = n => {
 Count binary Trailing Zeros
 @template {numeric} T
 @param {T} n
-@return {T}
 */
 export const ctz = n => ctztrim(n)[0]
 /**
@@ -64,7 +65,6 @@ strip all binary trailing zeros.
 intented as a high-speed alternative to `n >>= ctz(n)`
 @template {numeric} T
 @param {T} n
-@return {T}
 */
 export const trim = n => ctztrim(n)[1]
 
@@ -72,21 +72,31 @@ export const trim = n => ctztrim(n)[1]
 for educational purposes see: en.wikipedia.org/wiki/Hamming_weight#Efficient_implementation
 
 without optimization, it would be slow
-@param {numeric} x
-@return {numeric}
+@template {numeric} T
+@param {T} x
 */
 export const popCount = x => {
+	/**
+	@template {numeric} T
+	@param {T} x
+	*/
 	const popcnt = x => {
-		const B = isIntN(x); let c = B ? 0n : 0
-		while (x) { c += x & (B ? 1n : 1); x = B ? x >> 1n : x >>> 1 }
+		const B = /**@type {T extends bigint ? true : false}*/(typeof x == 'bigint')
+		const n1 = /**@type {T extends bigint ? 1n : 1}*/(B ? 1n : 1)
+		let c = /**@type {T extends bigint ? 0n : 0}*/(B ? 0n : 0)
+		while (x) {
+			//@ts-ignore
+			c += x & n1
+			x = /**@type {T}*/(B ? /**@type {bigint}*/(x) >> 1n : x >>> 1)
+		}
 		return c
 	}
-	if (isIntN(x)) return x < 0n ? Infinity : popcnt(x)
+	if (typeof x == 'bigint') return x < 0n ? Infinity : popcnt(x)
 	if (isInfNAN(x)) return NaN
 
 	x = abs(trunc(x))
 	//mantissa popcnt, because exponent doesn't matter
-	return Number(popcnt(castFloat2IntN(x) & nthMersenne(52n)) + 1n)
+	return Number(popcnt(castFloat2IntN(/**@type {number}*/(x)) & nthMersenne(52n)) + 1n)
 }
 
 /**
