0654e81cf604b33d481dbd0a5f4785c0b7a77d8a
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-12-11 17:45:55 -0400

    Update power.js

--- src/lib/power.js
+++ src/lib/power.js
@@ -1,31 +1,41 @@
-import {isBigInt as isIntN} from '../mod/type check'
-import {isInt} from '../mod/value check'
-import {abs} from '../mod/std'
-import {trunc} from './rounding'
-import {sizeOf, ctz} from '../mod/bitwise'
-import {root, sqrt, cbrt} from './root'
+import { isBigInt as isIntN } from '../mod/type check'
+import { isInt } from '../mod/value check'
+import { abs } from '../mod/std'
+import { trunc } from './rounding'
+import { sizeOf, ctz } from '../mod/bitwise'
+import { root, sqrt, cbrt } from './root'
 
 const lb = Math.log2
 
+/**
+check if it's a power of 2.
+
+1 is odd, so it's not a power of 2.
+It's a trivial power, because 1 is a power of any Real num
+
+@template T
+@param {T} x
+*/
 export const isPow2 = x => {
-	//1 is odd, so it's not a power of 2. It's a trivial power, because 1 is a power of any Real num
 	if (!isInt(x) || x < 2) return false
-	if (isIntN(x)) return !(x & (x - 1n))
-	return isInt(lb(x))
+	if (isIntN(x)) return !(/**@type {bigint}*/(x) & (/**@type {bigint}*/(x) - 1n))
+	return isInt(lb(/**@type {number}*/(x)))
 }
 
+/**
+@template T
+@param {T} x
+*/
 export const isSquare = x => {
 	if (!isInt(x)) return false
 	if (x < 2) return x >= 0
-	const c = ctz(x)
-	if (isIntN(x))
-	{
+	const c = ctz(/**@type {numeric}*/(x))
+	if (isIntN(x)) {
 		if (c & 1n) return false
-		x >>= c
+		/**@type {bigint}*/(x) >>= c
 		return (x & 7n) == 1n && sqrt(x) ** 2n == x
 	}
-	else
-	{
+	else {
 		if (c % 2) return false
 		x /= 2 ** c
 		return x % 8 == 1 && isInt(sqrt(x))
@@ -36,8 +46,7 @@ export const isCube = x => {
 	if (!isInt(x)) return false
 	if (!x) return true
 	const c = ctz(x)
-	if (isIntN(x))
-	{
+	if (isIntN(x)) {
 		if (c % 3n) return false
 		//the engine will probably reuse the shifted local copy of `n` inside `ctz`
 		x >>= c
@@ -50,17 +59,16 @@ export const isCube = x => {
 		However, this micro-algorithm is deprecated because computing the `abs` of a remainder < 9 is faster
 		*/
 		let m = abs(x % 9n); if (m > 1n && m != 8n) return false
-			m = abs(x % 7n); if (m > 1n && m != 6n) return false
+		m = abs(x % 7n); if (m > 1n && m != 6n) return false
 		return cbrt(x) ** 3n == x
 	}
-	else
-	{
+	else {
 		if ((x = abs(x)) < 2) return true
 		if (c % 3) return false
 		x /= 2 ** c
 		//https://math.stackexchange.com/a/2190888
 		let m = x % 9; if (m > 1 && m != 8) return false
-			m = x % 7; if (m > 1 && m != 6) return false
+		m = x % 7; if (m > 1 && m != 6) return false
 		return isInt(cbrt(x))
 	}
 }
@@ -71,8 +79,7 @@ export const isPow = x => {
 	//sign doesn't matter because Complex numbers exist
 	if ((x = abs(x)) < 4) return x < 2
 	if (isPow2(x) || isSquare(x) || isCube(x)) return true
-	if (isIntN(x))
-	{
+	if (isIntN(x)) {
 		const lbx = sizeOf(x, 1n, 0n)
 		for (let e = 5n; e < lbx; e += 2n) {
 			//we already discarded cubes, so we can skip all of them
@@ -81,8 +88,7 @@ export const isPow = x => {
 		}
 		return false
 	}
-	else
-	{
+	else {
 		const lbx = trunc(lb(x))
 		for (let e = 5; e < lbx; e += 2) {
 			if (!(e % 3)) continue
